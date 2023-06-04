f245250095d4b6bcbac36300d604a6c37233fd8f
extern-belenski-anton <belenskiyanton@gmail.com>
2023-06-04 14:19:26 +0200

    add HT files

--- results/hometask3.py
+++ results/hometask3.py
@@ -1,1 +1,16 @@
-#result file
+a = 1
+b = 2
+c = a + b
+print(c)
+#%%
+some_list = list()
+some_list = [-1, 4, 3, 7, 0]
+print(some_list.length)
+
+
+##for i in range list.count
+##if list[i] > list[i+1]
+
+#%%
+
+#%%
